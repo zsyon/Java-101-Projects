ad32d92cbcb6222b0e3aac2f7dc8b82cb633b9d5
HP <20060339@stu.omu.edu.tr>
2023-03-06 19:24:08 +0300

    numbers sorting calculation task

---
+++ Koşullu-İfadeler/Sayı-Sıralama/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+
+    int sayi1 = 10, sayi2 = 5, sayi3 = 3;
+
+    if(sayi1<sayi2 && sayi1<sayi3){
+      System.out.println("En kücük sayi " + sayi1);
+    }
+    else if(sayi2<sayi1 && sayi2<sayi3){
+      System.out.println("En küçük sayi " + sayi2);
+    }
+    else{
+      System.out.println("En küçük sayi " + sayi3);
+    }
+
+  }
+  
+}
