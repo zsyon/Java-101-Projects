e7219edb09d65afce6063cf93d171012d2b23510
HP <20060339@stu.omu.edu.tr>
2023-03-06 18:06:23 +0300

    complete VAT amount calculation task

---
+++ Temel-Kavramlar/KDV-Tutarı/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    double tutar, Kdv;
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Ücret Tutarını Giriniz : ");
+    tutar = input.nextDouble();
+
+    //double kdvli = (Kdv*tutar) + tutar;
+
+    boolean sonuc = tutar > 1000;
+
+    double kdvli = sonuc ? (0.08*tutar) + tutar : (0.18*tutar) + tutar;
+
+    double kdvTutari = kdvli - tutar;
+
+    System.out.println("KDV'siz Fiyat : " + tutar);
+    System.out.println("KDV'li Fiyat : " + kdvli);
+    System.out.println("KDV Tutarı : " + kdvTutari);
+    
+  }
+  
+}
