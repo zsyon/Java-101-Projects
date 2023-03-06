8fff204b3e3766f5006717ab88a7f46b0530fd96
HP <20060339@stu.omu.edu.tr>
2023-03-06 21:02:56 +0300

    perfect number calculation task

---
+++ Döngüler/Mükemmel-Sayı/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * mukemmelSayiBulanProgram
+ */
+public class Main {
+  public static void main(String[] args) {
+    
+    System.out.print("Lütfen sayı giriniz : ");
+    Scanner input = new Scanner(System.in);
+    int sayi = input.nextInt();
+    int toplam=0;
+
+    for(int i=1 ; i<sayi ; i++){
+
+      if(sayi%i == 0){
+
+        toplam += i;
+        
+      }
+    }
+
+    if(sayi==toplam){
+      System.out.println(sayi + " Mükemmel sayıdır");
+    }
+    else{
+      System.out.println(sayi + " Mükemmel sayı değildir");
+    }
+  }
+}
