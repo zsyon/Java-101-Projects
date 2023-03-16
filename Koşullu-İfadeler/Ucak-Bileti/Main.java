3ac0476e395a27b7bb8dc82a588ddd61f80b49a0
HP <20060339@stu.omu.edu.tr>
2023-03-17 01:16:29 +0300

    flight ticket calculation task

--- Koşullu-İfadeler/Ucak-Bileti/Main.java
+++ Koşullu-İfadeler/Ucak-Bileti/Main.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
 
-    int mesafe, yas, yolculukTipi;
+    int yas, yolculukTipi;
+    double mesafe;
 
     Scanner input = new Scanner(System.in);
 
     System.out.print("Mesafeyi km türünden giriniz : ");
-    mesafe = input.nextInt();
+    mesafe = input.nextDouble();
 
     System.out.print("Yaşınızı giriniz : ");
     yas = input.nextInt();
@@ -16,15 +17,32 @@ public class Main {
     System.out.print("Yolculuk tipini giriniz (1 => Tek Yön , 2 => Gidiş Dönüş ): ");
     yolculukTipi = input.nextInt();
 
-    if(mesafe>0 && yas>0 && yolculukTipi==2 ){
+    double tutar, indirimliTutar =0, yasİndirimi ;
+    tutar = mesafe * 0.10;
+
+    if(mesafe>0 && yas>0 && yolculukTipi==1 ){
+
+      if(yas<12){
+        yasİndirimi = tutar * 0.5;
+        indirimliTutar = tutar - yasİndirimi;
+
+      }
+      else if(yas>12 && yas<24){
+
+      }
+      else if(yas>65){
+
+      }
       
     }
-    else if(yolculukTipi==1){
+    else if(yolculukTipi==2){
       
     }
     else{
       System.out.println("Hatalı Giriş Yaptınız !");
     }
+
+    System.out.println(indirimliTutar);
     
   }
   
