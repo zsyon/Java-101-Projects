2066292f7c46dc6b24e10919defbe89c2446660a
HP <20060339@stu.omu.edu.tr>
2023-03-26 17:59:54 +0300

    combination calculation task

--- Döngüler/Kombinasyon-Hesaplama/Main.java
+++ Döngüler/Kombinasyon-Hesaplama/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
   public static void main(String[] args) {
 
-    int n, r;
-    
+    int n, r, n_faktoriyel=1, r_faktoriyel=1, faktoriyel=1;
+
     Scanner input = new Scanner(System.in);
 
     System.out.print("Lütfen n sayı giriniz : ");
@@ -15,6 +15,23 @@ public class Main {
 
     System.out.print("Lütfen r sayı giriniz : ");
     r = input.nextInt();
-    
+
+    for(int i=1 ; i<=n ; i++){
+      n_faktoriyel *= i;
+    }
+
+    for(int i=1 ; i<=r ; i++){
+      r_faktoriyel *= i;
+    }
+
+    for(int i=1 ; i<=(n-r) ; i++){
+      faktoriyel *= i;
+    }
+
+    double sonuc;
+    sonuc = n_faktoriyel / (r_faktoriyel * faktoriyel);
+
+    System.out.println("Kombinasyon Sonucu = " + sonuc);
+
   }
 }
