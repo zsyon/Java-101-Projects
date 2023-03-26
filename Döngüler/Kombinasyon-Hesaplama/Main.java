6d7d9b82c2d15a528e3e005d10569bf6e6bd8563
HP <20060339@stu.omu.edu.tr>
2023-03-26 17:45:04 +0300

    combination calculation task

--- Döngüler/Kombinasyon-Hesaplama/Main.java
+++ Döngüler/Kombinasyon-Hesaplama/Main.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class Main {
 
   public static void main(String[] args) {
+
+    int n, r;
+    
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Lütfen n sayı giriniz : ");
+    n = input.nextInt();
+
+    System.out.print("Lütfen r sayı giriniz : ");
+    r = input.nextInt();
     
   }
 }
