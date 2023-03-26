bbc70c5c5481e25901c4d3b0172063fc012d48f2
HP <20060339@stu.omu.edu.tr>
2023-03-27 00:24:16 +0300

    exponential calculation task

---
+++ Döngüler/Üslü-Sayı/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+/**
+ * Main
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+    int us, taban;
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Lütfen taban sayı giriniz : ");
+    taban = input.nextInt();
+
+    System.out.print("Lütfen üs sayı giriniz : ");
+    us = input.nextInt();
+
+  }
+}
