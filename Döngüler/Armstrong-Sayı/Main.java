65d939d253d505ebed2068b0b015b73c35c1039d
HP <20060339@stu.omu.edu.tr>
2023-03-29 00:21:27 +0300

    add README.md

--- Döngüler/Armstrong-Sayı/Main.java
+++ Döngüler/Armstrong-Sayı/Main.java
@@ -13,6 +13,8 @@ public class Main {
     System.out.print("Lütfen sayı giriniz : ");
     sayi = input.nextInt();
 
+    System.out.println(sayi);
+
     int birler_basamagi = sayi%10 ;
 
     System.out.println(birler_basamagi);
---
+++ Metotlar/Fibonacci-Serisi/README.md
@@ -0,0 +1,3 @@
+# Recursive ile Fibonacci Serisi Bulan Program
+
+Java'da recursive metotlar ile fibonacci serisi bulan program yapıyoruz.
