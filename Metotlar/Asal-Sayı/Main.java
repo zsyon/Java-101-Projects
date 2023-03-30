3ef30b2ae3c78a38569297c23f6a80c185c83e98
HP <20060339@stu.omu.edu.tr>
2023-03-30 23:30:07 +0300

    prime number calculation task

--- Metotlar/Asal-Sayı/Main.java
+++ Metotlar/Asal-Sayı/Main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 
+  static String f(int n){
+
+    for(int i=2 ; i<10 ; i++){
+      if(n%i == 0){
+        return n + "sayısı ASALDIR !";
+      }
+      else {
+        return n + "sayısı ASAL DEĞİLDİR !";
+      }
+    }
+
+    return "a";
+  }
+
   public static void main(String[] args) {
 
     int sayi;
@@ -10,8 +24,8 @@ public class Main {
     System.out.print("Sayı giriniz : ");
     sayi = input.nextInt();
 
-    
+    System.out.println(f(sayi));
 
-    
+
   }
 }
