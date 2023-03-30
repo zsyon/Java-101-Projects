81a1fcf553e22fa55e9f04c129b46d12c329ce06
HP <20060339@stu.omu.edu.tr>
2023-03-30 23:02:07 +0300

    prime number calculation task

---
+++ Metotlar/Asal-Sayı/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    int sayi;
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Sayı giriniz : ");
+    sayi = input.nextInt();
+
+    
+
+    
+  }
+}
